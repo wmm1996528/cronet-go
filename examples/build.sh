2ebe9d685a287acf3dd0f616bce7b3141e9a6832
Your Name <you@example.com>
2024-05-05 11:50:41 +0000

    1

--- examples/build.sh
+++ examples/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-export set CGO_CFLAGS="-I/Users/wang/chromium/src/out/Cronet/cronet/include"
-export set CGO_LDFLAGS="-Wl,-rpath,/Users/wang/chromium/src/out/Cronet/cronet -L/Users/wang/chromium/src/out/Cronet/cronet -lcronet"
-
+export set CGO_CFLAGS="-I/home/wang/cronet-go/examples/cron"
+export set CGO_LDFLAGS="-Wl,-rpath,/home/wang/cronet-go/examples/cron -L/home/wang/cronet-go/examples/cron -lcronet"
+export GOPROXY=https://goproxy.cn
 go build -o ./example1 ./1
 go build -o ./example2 ./2
 go build -o ./example3 ./3
