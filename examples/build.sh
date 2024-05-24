58dd6ce68a3639b97901432b33daca39575deb73
wang <912594746@qq.com>
2024-05-24 18:13:14 +0800

    111

--- examples/build.sh
+++ examples/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 #export set CGO_CFLAGS="-I"
-export set CGO_CFLAGS="-I/Users/wang/Desktop/cronet-go/examples/cronet/include"
+export set CGO_CFLAGS="-I/Users/wang/Desktop/cronet-binaries/src/out/Release/cronet"
 #export set CGO_LDFLAGS="-Wl,-rpath, -L -lcronet"
-export set CGO_LDFLAGS="-Wl,-rpath,/Users/wang/Desktop/cronet-go/examples/cronet -L/Users/wang/Desktop/cronet-go/examples/cronet -lcronet"
+export set CGO_LDFLAGS="-Wl,-rpath,/Users/wang/Desktop/cronet-binaries/src/out/Release/cronet -L/Users/wang/Desktop/cronet-binaries/src/out/Release/cronet -lcronet"
 export GOPROXY=https://goproxy.cn
 go build -o ./example1 ./1
-go build -o ./example2 ./2
-go build -o ./example3 ./3
+#go build -o ./example2 ./2
+#go build -o ./example3 ./3
--- go.mod
+++ go.mod
@@ -2,12 +2,13 @@ module github.com/wmm1996528/cronet-go
 
 go 1.21
 
+//require github.com/sagernet/sing v0.0.0-20220614131337-ea019b365507
+
 require (
 	github.com/elazarl/goproxy v0.0.0-20231117061959-7cc037d33fb5 // indirect
 	github.com/elazarl/goproxy/ext v0.0.0-20231117061959-7cc037d33fb5 // indirect
 	github.com/rogpeppe/go-charset v0.0.0-20180617210344-2471d30d28b4 // indirect
+	github.com/weblifeio/cronet-go v0.0.0-20240501155250-bdd31ebbf0f7 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	gopkg.in/elazarl/goproxy.v1 v1.0.0-20180725130230-947c36da3153 // indirect
-	github.com/sagernet/sing v0.0.0-20220614131337-ea019b365507
-
 )
